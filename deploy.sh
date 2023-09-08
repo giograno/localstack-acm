42b9d3e47659c665dc5383e58659b4fb50f530eb
Giovanni Grano <me@giograno.com>
2023-09-08 16:10:19 -0400

    init

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # deploy route 53 hosted zone
-hosted-id=$(awslocal route53 create-hosted-zone --name blabla.com --caller-reference 2022-01-03-1 | jq "HostedZone.Id"
+hosted_id=$(awslocal route53 create-hosted-zone --name blabla.com --caller-reference 2022-01-03-1 | jq -r ".HostedZone.Id")
+export TF_VAR_hosted_id=$hosted_id
+echo $TF_VAR_hosted_id
 
 rm -rf .terraform*
 rm -rf terraform*
--- main.tf
+++ main.tf
@@ -1,22 +1,9 @@
-# provider "aws" {
-#   access_key                  = "mock_access_key"
-#   secret_key                  = "mock_secret_key"
-#   region                      = "us-east-1"
-#   s3_use_path_style           = true
-#   skip_credentials_validation = true
-#   skip_metadata_api_check     = true
-#   skip_requesting_account_id  = true
-
-#   endpoints {
-#     route53        = "http://localhost:4566"
-#     acm            = "http://localhost:4566"
-#   }
-# }
+variable hosted_id { type = string }
 
 module "ssl-cert" {
   source = "terraform-aws-modules/acm/aws"
   version = "4.3.2"
 
   domain_name = "blabla.com"
-  zone_id = "/hostedzone/VVFU9S0V11WHXP4"
+  zone_id = var.hosted_id
 }
